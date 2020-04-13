5aee28eacdbc16b32643ab202bbfe0713b9b5d6c
lossitomatossi <tomas.terala@gmail.com>
2020-04-13 11:48:09 +0300

    step2-step4

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -14,18 +14,32 @@ const Button = (props) => (
 )
 
 const Statistics = (props) => {
-  const x = 0
-
-  return (
-    <>
-      <h1>Statistics</h1>
-      
-        <p>Good: {props.good}</p>
-        <p>Neutral: {props.neutral}</p>
-        <p>Bad: {props.bad}</p>
-      
-    </>
-  )
+  const all = props.good+props.neutral+props.bad
+  const positive = props.good / all
+  const average = (props.good+props.bad*(-1))/all
+  if (all > 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        
+          <p>good {props.good}</p>
+          <p>neutral {props.neutral}</p>
+          <p>bad {props.bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive}%</p>
+        
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+  
 }
 
 
@@ -35,12 +49,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad + 1
 
   const setToValueGood = (newValue) => {
     setGood(newValue)
     console.log("good feedbacks: ",newValue)
-    console.log("total feedbacks: ",all)
   }
 
   const SetToValueNeutral = (newValue) => {
@@ -61,7 +73,7 @@ const App = () => {
       <Button handleClick={() => SetToValueBad(bad + 1)} text="bad" />
       <Button handleClick={() => SetToValueNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setToValueGood(good + 1)} text="good" />
-      <Statistics good = {good} neutral = {neutral} bad = {bad} all = {all}/>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
 
     </div>
   )
