262c542c762a3b73a2505a72893aad4b8351b641
lossitomatossi <tomas.terala@gmail.com>
2020-04-13 11:33:34 +0300

    step1 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -13,16 +13,34 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  const x = 0
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+      
+    </>
+  )
+}
+
+
 
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad + 1
 
   const setToValueGood = (newValue) => {
     setGood(newValue)
     console.log("good feedbacks: ",newValue)
+    console.log("total feedbacks: ",all)
   }
 
   const SetToValueNeutral = (newValue) => {
@@ -43,7 +61,7 @@ const App = () => {
       <Button handleClick={() => SetToValueBad(bad + 1)} text="bad" />
       <Button handleClick={() => SetToValueNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setToValueGood(good + 1)} text="good" />
-      
+      <Statistics good = {good} neutral = {neutral} bad = {bad} all = {all}/>
 
     </div>
   )
