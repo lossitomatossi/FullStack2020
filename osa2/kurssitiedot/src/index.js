bd94ef6ff0510a67fd6db714ea68b04940aea663
lossitomatossi <tomas.terala@gmail.com>
2020-04-19 19:57:15 +0300

    2.1 tehty

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { element } from 'prop-types';
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header header={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
 
 const Header = (props) => {
   return (
@@ -12,21 +20,27 @@ const Header = (props) => {
 
 const Content = (props) => {
   console.log(props)
+  const { parts } = props
+  console.log("content saa tiedot: ", parts)
+  
+  
   return (
     <div>
-      <Part part = {props.parts[0]}/>
-      <Part part = {props.parts[1]}/>
-      <Part part = {props.parts[2]}/>
+      <ul>
+        {parts.map(part =>
+         <li key={part.id}>
+           <Part part = {part} />
+         </li>)}
+      </ul>
     </div>
   )
 }
 
+
 const Part = (props) => {
   return (
     <>
-      <p>
         {props.part.name} {props.part.exercises}
-      </p>
     </>
   )
 }
@@ -42,27 +56,30 @@ const Total = (props) => {
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name:'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
      }
    ]
   }
 
   return (
     <div>
-      <Header header={course.name}/>
-      <Content parts={course.parts}/>
       <Total total={course.parts}/>
+      <Course course={course}/>
     </div>
     
   )
