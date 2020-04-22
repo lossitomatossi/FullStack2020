b0d7df5461687e51ef9a52bebd8139903efc6bee
lossitomatossi <tomas.terala@gmail.com>
2020-04-22 14:52:37 +0300

    2.6 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,20 +4,44 @@ const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newPerson, setNewPerson ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newPerson,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewPerson('')
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewPerson(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+           value = {newPerson}
+           onChange = {handlePersonChange}
+           />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
+        <ul>
+          {persons.map(person =>
+            <li key={person.name}>{person.name}</li>)}
+        </ul>
+      </div>
       ...
     </div>
   )
