6a7e0458aba7abd8e5a4d2a454fda9209c6bb02f
lossitomatossi <tomas.terala@gmail.com>
2020-04-23 18:29:02 +0300

    2.8 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newPerson, setNewPerson ] = useState('')
+  const [newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newPerson,
+      number: newNumber,
     }
 
     const personExists = persons.some(p => p.name === newPerson)
@@ -22,8 +27,13 @@ const App = () => {
   }
 
   const handlePersonChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewPerson(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -37,6 +47,12 @@ const App = () => {
            />
         </div>
         <div>
+          number: <input
+          value = {newNumber}
+          onChange = {handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -44,10 +60,9 @@ const App = () => {
       <div>
         <ul>
           {persons.map(person =>
-            <li key={person.name}>{person.name}</li>)}
+            <li key={person.name}>{person.name} {person.number}</li>)}
         </ul>
       </div>
-      ...
     </div>
   )
 
