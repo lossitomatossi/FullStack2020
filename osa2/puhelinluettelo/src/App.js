cd4989045343add8ca3bbfaeb15de1b571e8a5c8
lossitomatossi <tomas.terala@gmail.com>
2020-04-24 17:52:17 +0300

    persons eriytetty app:n ulkopuolelle

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,40 @@
 import React, { useState } from 'react'
 
+const Filter = (props) => {
+  const [newFilter, setNewFilter ] = useState('')
+  
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+    const filt = props.persons.some(p => p.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    if (filt && event.target.value !== '') {
+      console.log(`${event.target.value} matches to something`)
+    } else {
+      console.log(`${event.target.value} Ei osu`)
+    }
+  }
+  return (
+    <>
+      filter shown with: <input
+        value = {newFilter}
+        onChange = {handleFilterChange}
+      />
+    </>
+  )
+}
+
+const Persons = (props) => {
+    return (
+    <>
+      <ul>
+      {props.persons.map(person =>
+        <li key={person.name}>{person.name} {person.number}</li>)}
+      </ul>
+    </>
+  )
+}
+
+
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -9,6 +44,7 @@ const App = () => {
   ]) 
   const [ newPerson, setNewPerson ] = useState('')
   const [newNumber, setNewNumber ] = useState('')
+  
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,9 +72,16 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <Filter persons={persons}/>
+      </div>
+
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -56,13 +99,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <div>
-        <ul>
-          {persons.map(person =>
-            <li key={person.name}>{person.name} {person.number}</li>)}
-        </ul>
-      </div>
+      <h3>Numbers</h3>
+      <Persons persons={persons} />
     </div>
   )
 
