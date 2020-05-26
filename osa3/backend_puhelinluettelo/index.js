b8f85c9709846de16ffef9bed45c3ed22159f132
lossitomatossi <tomas.terala@gmail.com>
2020-05-26 20:57:28 +0300

    3.4 tehty, requests kansio pystyssä

--- osa3/backend_puhelinluettelo/index.js
+++ osa3/backend_puhelinluettelo/index.js
@@ -10,7 +10,7 @@ let persons = [
     {
         name: "Ada Lovelace",
         number: "39-44-5323523",
-        id: 4
+        id: 2
     },
     {
         name: "Dan Abramov",
@@ -21,6 +21,11 @@ let persons = [
         name: "Mary Poppendieck",
         numbeid: "39-23-6423122",
         id: 4
+    },
+    {
+        name: "Testi",
+        number: "pepperoni",
+        id: 5
     }
 ]
 
@@ -45,6 +50,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
