2f2e6857f2cdc3b69a5f51ea5dae4ac915ada068
lossitomatossi <tomas.terala@gmail.com>
2020-05-26 19:42:22 +0300

    3.1 tehty

--- osa3/backend_puhelinluettelo/index.js
+++ osa3/backend_puhelinluettelo/index.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const app = express()
 
+let notes = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 4
+    },
+    {
+        name: "Dan Abramov",
+        numbeid: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        numbeid: "39-23-6423122",
+        id: 4
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(notes)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
